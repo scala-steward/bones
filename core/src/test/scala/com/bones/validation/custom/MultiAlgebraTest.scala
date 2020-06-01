85e58bed3f467627ce4cedcd979215f169403bfd
Travis Stevens <travis.stevens@gaia.com>
2020-06-01 11:23:21 -0400

    added just a little bit to the COV test

--- core/src/test/scala/com/bones/validation/custom/MultiAlgebraTest.scala
+++ core/src/test/scala/com/bones/validation/custom/MultiAlgebraTest.scala
@@ -13,7 +13,7 @@ class MultiAlgebraTest extends AnyFunSuite with Checkers {
     ("email", email()) ::
     ("instant", instant(jt_i.min(Instant.parse("2007-12-03T10:15:30.00Z")))) ::
     ("description", string) ::
-    kvpNil[AllCustomAlgebras]
+    kvpNil
 
   case class MultiAlgebra(email: String, instant: Instant, description: String)
 
--- interchange-format-interpreters/circe/src/test/scala/com/bones/circe/CovCirceTest.scala
+++ interchange-format-interpreters/circe/src/test/scala/com/bones/circe/CovCirceTest.scala
@@ -5,14 +5,13 @@ import java.time.{Instant, ZoneId}
 import java.util.Locale
 
 import com.bones.circe.custom.BaseScalaCoreEncoder
-import com.bones.data.custom.ScalaCoreValue
 import com.bones.interpreter.KvpInterchangeFormatEncoderInterpreter.InterchangeFormatEncoder
 import com.bones.schemas.CustomCovSchema._
 import io.circe.Json
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.must.Matchers
 import org.scalatestplus.scalacheck.Checkers
-import shapeless.{:+:, CNil, Inl, Inr}
+import shapeless.{Inl, Inr}
 
 class CovCirceTest extends AnyFunSuite with Checkers with Matchers {
 
@@ -20,9 +19,6 @@ class CovCirceTest extends AnyFunSuite with Checkers with Matchers {
     PropertyCheckConfiguration(minSuccessful = 1000, workers = 5)
 
   test("scalacheck allSupport types - marshall then unmarshall with custom algebra") {
-    val validateFromCirce = IsoCirceEncoderAndValidatorInterpreter
-
-    //  val jsonToCc = validateFromCirce.byteArrayFuncFromSchema(allSupportCaseClass, Charset.forName("UTF8"))
 
     val dateFormatter: DateTimeFormatter =
       DateTimeFormatter
@@ -60,20 +56,25 @@ class CovCirceTest extends AnyFunSuite with Checkers with Matchers {
     val blogPostToJson = IsoCirceEncoderAndValidatorInterpreter
       .encoderFromCustomSchema(BlogPost.blogPostSchema, BlogEncoder)
 
-    val blogPost = BlogPost(1, "title", List("tag1", "tag2"), Instant.now(), "Here is some content")
+    val instant = Instant.parse("2020-12-03T10:15:30.00Z")
+
+    val blogPost = BlogPost(1, "title", List("tag1", "tag2"), instant, "Here is some content")
 
     val json = blogPostToJson.apply(blogPost)
     val jsonString = json.spaces2
 
-//    println(jsonString)
+    val expectedResult = """{
+                           |  "id" : 1,
+                           |  "title" : "title",
+                           |  "tags" : [
+                           |    "tag1",
+                           |    "tag2"
+                           |  ],
+                           |  "publishDate" : "20201203101530.0Z",
+                           |  "content" : "Here is some content"
+                           |}""".stripMargin
 
-//    val newCc = jsonToCc(jsonString)
-//    newCc match {
-//      case Left(x) =>
-//        fail(s"expected success, received $x for JSON string ${io.circe.parser.parse(new String(jsonString, utf8))}")
-//      case Right(newCc2) =>
-//        newCc2.fancyEquals(cc)
-//    }
+    jsonString mustEqual expectedResult
 
   }
 
