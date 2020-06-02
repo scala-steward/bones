341047cff4834632314cfa81c2a1616c2775541b
Travis Stevens <travis.stevens@gaia.com>
2020-06-02 00:21:07 -0400

    refactored argonaut interpreter

--- interchange-format-interpreters/argonaut/src/main/scala/com/bones/argonaut/package.scala
+++ interchange-format-interpreters/argonaut/src/main/scala/com/bones/argonaut/package.scala
@@ -1,11 +1,5 @@
 package com.bones
 
-import java.time.format.DateTimeFormatter
-
-import _root_.argonaut.Json
-import com.bones.interpreter.KvpInterchangeFormatEncoderInterpreter.DateToStringEncoder
-import com.bones.interpreter.KvpInterchangeFormatValidatorInterpreter.StringToDateValidator
-
 package object argonaut {
 
   /** An implementation of an Argonaut Encoder and and Validator using Standard ISO DATE string
